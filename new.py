56867c9a527538bff9b8508baba349c91f391c38
rusingh <rupam.singh@aau.at>
2023-01-10 14:53:22 +0100

    new changes in repo

---
+++ new.py
@@ -0,0 +1,6 @@
+'fsjkdhf'
+import test as t
+
+name = input("anything")
+name = t.last(name)
+print(name)
---
+++ test.py
@@ -0,0 +1,14 @@
+
+'Module providingFunction printing python version'
+# code for name
+
+
+def last(name):
+    "Functio info."
+    return name + ' is cool '
+
+
+def __main__():
+    your_name = input('please tell your name:')
+    your_name = last(your_name)
+    print(your_name)
